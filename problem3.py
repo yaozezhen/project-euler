e08b6cbe73bd732e3b1b4ad1556f9d1492bfa8bf
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-05-09 03:19:14 -0400

    Create problem3.py

---
+++ problem3.py
@@ -0,0 +1,23 @@
+def checkPrime(n): 
+  p = 2
+  while p*p <= n: 
+    if (n % p == 0):
+      return False
+    p += 1
+  return True
+
+#print(checkPrime(100))
+
+def largestPrimeFactor(n): 
+  f = 2
+  num = n
+  while f*f <= num:  
+    if (num % f == 0): 
+      num /= f
+      #if (checkPrime(num) == True): 
+      #  return int(num)
+    else: 
+      f += 1
+  return int(num)
+
+print(largestPrimeFactor(600851475143))
