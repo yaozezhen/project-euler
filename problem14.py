f1a4dbb9405c33a8b93fdbcd139e02e2e526f717
yaozezhen <63181901+yaozezhen@users.noreply.github.com>
2020-05-23 00:56:42 -0400

    Update problem14.py

--- problem14.py
+++ problem14.py
@@ -9,13 +9,13 @@ def collatzSeq(n):
     count += 1
   return count
 
-#print(collatzSeq(9))
-
 def longestCollatz(n): 
   maxLen = 1
   for i in range(1,n): 
-    if (collatzSeq(i) > maxLen): 
-      maxLen = collatzSeq(i)
-  return maxLen
+    collatz = collatzSeq(i)
+    if (collatz > maxLen): 
+      maxLen = collatz
+      colNum = i
+  return colNum
 
 print(longestCollatz(1000000))
